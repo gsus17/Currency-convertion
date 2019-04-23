42bed066d277e85bfb0833b2e572e5363f9554e6
Suarez Jesus <suarezmolina17@gmail.com>
2019-04-22 23:42:22 -0300

    Fix api request and symbols.

--- src/app/api-wrapper.service.ts
+++ src/app/api-wrapper.service.ts
@@ -13,15 +13,7 @@ export class ApiWrapperService {
    */
   public getList() {
     const url: string = 'http://data.fixer.io/api/latest?access_key=33b23d6e01efe285daf21f65e1124757';
-    const httpOptions = {
-      headers: new HttpHeaders()
-        .set('Access-Control-Allow-Origin', '*')
-        .set('Access-Control-Allow-Headers', '*')
-        .set('Content-Type', 'application/json')
-        .set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-    };
-
-    return this.httpClient.get(url, httpOptions).toPromise();
+    return this.httpClient.get(url).toPromise();
   }
 
 }
